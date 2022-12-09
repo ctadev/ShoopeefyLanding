3ccc995e077d9b47630a90da40743739dda4f9ec
ctadev <53548168+ctadev@users.noreply.github.com>
2022-12-09 14:59:21 -0500

    fixed grid layout

--- src/components/gridMenu/GridLayout.jsx
+++ src/components/gridMenu/GridLayout.jsx
@@ -9,11 +9,11 @@ const data = [
   },
   {
     title: "Children",
-    img: "https://images.pexels.com/photos/1813947/pexels-photo-1813947.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    img: "https://images.pexels.com/photos/818992/pexels-photo-818992.jpeg?auto=compress&cs=tinysrgb&w=1600",
   },
   {
     title: "Sales",
-    img: "https://images.pexels.com/photos/818992/pexels-photo-818992.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    img: "https://images.pexels.com/photos/1813947/pexels-photo-1813947.jpeg?auto=compress&cs=tinysrgb&w=1600",
   },
   {
     title: "Gifts",
@@ -43,7 +43,7 @@ export default GridLayout;
 const s = {
   grid: "grid grid-flow-dense grid-cols-2 md:grid-cols-3 xl:grid-cols-gridLayoutColumn auto-rows-autoRow300 max-w-[1200px] gap-2 mx-auto mt-16 shadow-lg",
   cards:
-    "relative [&:nth-child(2)]:row-span-2 [&:nth-child(4)]:col-span-2 md:[&:nth-child(4)]:col-span-1 md:last:col-span-3 xl:last:col-span-2",
+    "relative [&:nth-child(2)]:row-span-2 [&:nth-child(4)]:col-span-2 md:[&:nth-child(4)]:col-span-1 md:last:row-span-2 xl:last:col-span-1 xl:last:row-span-1",
   img: "w-full h-full object-cover",
   info: "absolute left-[50%] top-[50%] translate-x-[-50%] translate-y-[-50%] bg-sky-200 py-2 px-4 cursor-pointer",
 };
